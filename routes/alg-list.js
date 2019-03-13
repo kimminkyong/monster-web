134d988b8396ca2c9926ccb11c666c8ce5109975
kimminkyong <kmk1572@gmail.com>
2019-03-13 09:33:10 +0900

    ls

--- routes/alg-list.js
+++ routes/alg-list.js
@@ -7,24 +7,21 @@ var connection = mysql.createConnection(dbconfig);
 
 /* GET home page. */
 router.get('/:date', function(req, res, next) {
-  //res.render('alg-list', { title: 'ALGORITHM LIST', rows: rows });
-  connection.connect((err) =>{
-    if(err){
-      console.log(err)
-      return
-    }
-    console.log('mysql connect completed');
-  })
-  
-  connection.query('SELECT * from MA01 WHERE date="'+req.params.date+'"', function(err, rows) {
-    console.log(req.params.date);
-    if(err) throw err;
-    res.render('alg-list', { title: 'ALGORITHM LIST', rowList : rows ,listDate:req.params.date});
-    console.log('The solution is: ', rows);
-    //res.send(rows);
-    //res.render('alg-list', { title: rows });
-  });
+    //connection.connect((err) =>{
+        // if(err){
+        //   console.log(err)
+        //   return
+        // }
 
+        connection.query('SELECT * from MA01 WHERE date="'+req.params.date+'"', function(err, rows) {
+          if(err) throw err;
+          res.render('alg-list', { title: 'ALGORITHM LIST', rowList : rows ,listDate:req.params.date});
+          console.log('The solution is: ', rows);
+        });
+
+        console.log('mysql connect completed!!');
+    // });
+    // connection.end();
 });
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,59 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql = require('mysql');
+var dbconfig = require('../config/database.js');
+var connection = mysql.createConnection(dbconfig);
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
+function monsterA01Log(req, res, next){
+  connection.query('SELECT * from monster_log WHERE log_kind="a01_log" ORDER BY date DESC', function(err, rows) {
+    if(err) throw err;
+    // res.render('index', { title: 'Express', a01LogList : rows });
+    req.a01LogList = rows
+    console.log(rows);
+    next();
+  });
+
+}
+
+function monsterCodeLog(req, res, next){
+  connection.query('SELECT * from monster_log WHERE log_kind="d_log" ORDER BY date DESC', function(err, rows) {
+    if(err) throw err;
+    // res.render('index', { title: 'Express', a01LogList : rows });
+    req.codeLogList = rows
+    console.log(rows);
+    next();
+  });
+
+}
+
+
+function showPageRender(req, res){
+  res.render('index', {
+    title: 'Express',
+    a01LogList: req.a01LogList,
+    codeLogList: req.codeLogList
+  });
+}
+
+router.get('/', monsterA01Log, monsterCodeLog, showPageRender );
+  //res.render('index', { title: 'Express' });
+
+
+
+  // connection.query('SELECT * from monster_log WHERE log_kind="a01_log" ORDER BY date DESC', function(err, rows) {
+  //   if(err) throw err;
+  //   res.render('index', { title: 'Express', a01LogList : rows });
+  //   console.log(rows);
+  // });
+
+
+
+
+
+
 
 
 module.exports = router;
