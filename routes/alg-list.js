012b5070a252d5a8e117bc264576cac378d30d0d
kimminkyong <kmk1572@gmail.com>
2019-03-13 15:39:13 +0900

    0313-1

--- routes/alg-list.js
+++ routes/alg-list.js
@@ -7,22 +7,39 @@ var connection = mysql.createConnection(dbconfig);
 
 /* GET home page. */
 router.get('/:date', function(req, res, next) {
-    //connection.connect((err) =>{
-        // if(err){
-        //   console.log(err)
-        //   return
-        // }
+    var itemTotalLength = 0;
+    var step1Length = 0;
+    var step2Length = 0;
 
-        connection.query('SELECT * from MA01 WHERE date="'+req.params.date+'"', function(err, rows) {
-          if(err) throw err;
-          res.render('alg-list', { title: 'ALGORITHM LIST', rowList : rows ,listDate:req.params.date});
-          console.log('The solution is: ', rows);
-        });
+    var totalLength='SELECT count(*) as total from TOTAL_STOCK_CODE';
+    connection.query(totalLength, function(err, rows) {
+      if(err) throw err;
+      itemTotalLength = rows[0].total;
+    });
 
-        console.log('mysql connect completed!!');
+    var s2Length='SELECT count(*) as total from MA01 WHERE alg_step="step2"';
+    connection.query(s2Length, function(err, rows) {
+      if(err) throw err;
+      step2Length = rows[0].total;
+    });
 
-    // });
-    // connection.end();
+    var qs='SELECT * from MA01 JOIN TOTAL_STOCK_CODE ON MA01.code = TOTAL_STOCK_CODE.code WHERE MA01.date="'+req.params.date+'"';
+    connection.query(qs, function(err, rows) {
+      if(err) throw err;
+      step1Length = rows.length;
+      res.render('alg-list', { 
+        title: 'ALGORITHM LIST', 
+        rowList : rows,
+        listDate : req.params.date, 
+        total_item_length: itemTotalLength,
+        step1_length: step1Length,
+        step2_length: step2Length
+      });
+      console.log('The solution is: ', rows);
+    });
+
+    console.log('mysql connect completed!!');
+
 });
 
 
